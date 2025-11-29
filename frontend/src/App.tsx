6517ce8bfd2ca5a77413c4e21cd25ddab9443b74
sel-hasn <fangyuansamach@gmail.com>
2025-11-29 14:53:12 +0100

    fix(frontend): fixing format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/auth/LoginPage";
-import GamePage from "./pages/game/GamePage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/auth/LoginPage';
+import GamePage from './pages/game/GamePage';
 
 function App(): JSX.Element {
   return (
