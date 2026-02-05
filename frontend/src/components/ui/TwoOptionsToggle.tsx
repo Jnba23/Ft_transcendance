c6a87e4357ae72b5024e314c0dd11b6468990f14
yikenyiken <youssf.iken@gmail.com>
2026-02-05 22:33:03 +0100

    refactor<ui>: add support for callback per button-click in TwoOptionsToggle

--- frontend/src/components/ui/TwoOptionsToggle.tsx
+++ frontend/src/components/ui/TwoOptionsToggle.tsx
@@ -3,28 +3,43 @@ import React from 'react';
 type TwoOptionsToggleProps = {
 	opt1: string,
 	opt2: string,
-	setRequestType?: (type: string) => void, // make type boolean or keyof something
+	onOpt1Select?: () => void,
+	onOpt2Select?: () => void,
 };
 
-function TwoOptionsToggle({ opt1, opt2, setRequestType }: TwoOptionsToggleProps) {
+function TwoOptionsToggle({
+	opt1,
+	opt2,
+	onOpt1Select,
+	onOpt2Select
+}: TwoOptionsToggleProps) {
+
 	const [isActive, setIsActive] = React.useState(false);
 
 	return (
-		<div className="flex gap-2 items-center bg-black/20 rounded-lg p-1"> {/*TwoOptionToggle <ui> */}
-			<button className={[
-				"flex-1 py-1.5 px-3 rounded-md transition-colors",
-				"text-sm font-semibold text-white/60",
-				"hover:bg-white/10 hover:text-white",
-				"aria-pressed:bg-primary aria-pressed:text-white",
-			].join(' ')} onClick={() => setIsActive(true)} aria-pressed={isActive}>
+		<div className="flex gap-2 items-center bg-black/20 rounded-lg p-1">
+			<button
+				className={[
+					"flex-1 py-1.5 px-3 rounded-md transition-colors",
+					"text-sm font-semibold text-white/60",
+					"hover:bg-white/10 hover:text-white",
+					"aria-pressed:bg-primary aria-pressed:text-white",
+				].join(' ')}
+				onClick={() => { setIsActive(true); onOpt1Select?.(); }}
+				aria-pressed={isActive}
+			>
 				{opt1}
 			</button>
-			<button className={[
-				"flex-1 py-1.5 px-3 rounded-md transition-colors",
-				"text-sm font-semibold text-white/60",
-				"hover:bg-white/10 hover:text-white",
-				"aria-pressed:bg-primary aria-pressed:text-white",
-			].join(' ')} onClick={() => setIsActive(false)} aria-pressed={!isActive}>
+			<button
+				className={[
+					"flex-1 py-1.5 px-3 rounded-md transition-colors",
+					"text-sm font-semibold text-white/60",
+					"hover:bg-white/10 hover:text-white",
+					"aria-pressed:bg-primary aria-pressed:text-white",
+				].join(' ')}
+				onClick={() => { setIsActive(false); onOpt2Select?.(); }}
+				aria-pressed={!isActive}
+			>
 				{opt2}
 			</button>
 		</div>
