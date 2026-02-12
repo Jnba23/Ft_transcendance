90e7ed56273539fde956a2fec75745435e0cae62
yikenyiken <youssf.iken@gmail.com>
2026-02-12 13:45:42 +0100

    feat<sidebar>: add View Profile click behavior

--- frontend/src/components/layout/Sidebar/sections/shared/UserOptions.tsx
+++ frontend/src/components/layout/Sidebar/sections/shared/UserOptions.tsx
@@ -1,25 +1,30 @@
+import { useNavigate } from "react-router-dom";
 import UserOptsItem from './UserOptsItem';
 
 type UserOptionsProps = {
   isOpen: boolean;
   isFriend?: boolean;
+  user_id: number
 };
 
-function UserOptions({ isOpen, isFriend }: UserOptionsProps) {
+function UserOptions({ isOpen, isFriend, user_id }: UserOptionsProps) {
+  const navigate = useNavigate();
+  const toProfile = () => {
+    navigate(`/profile/${user_id}`);
+  }
+
   return (
     <div style={isOpen ? undefined : { display: 'none' }}>
       <div
         className={[
-          'px-2 pb-2 mt-1 pt-1',
+          'px-2 pb-2 pt-1',
           'flex gap-1 flex-col',
           'border-t border-white/5',
         ].join(' ')}
       >
-        <UserOptsItem icon="visibility" label="View Profile" />
-        <UserOptsItem icon="block" label="Block User" />
-        <UserOptsItem icon="sports_esports" label="Send Game Request" />
+        <UserOptsItem icon="visibility" label="View Profile" onClick={toProfile}/>
         {!isFriend && (
-          <UserOptsItem icon="person_add" label="Send Friend Request" />
+          <UserOptsItem icon="person_add" label="Send Friend Request" onClick={() => 0}/>
         )}
       </div>
     </div>
