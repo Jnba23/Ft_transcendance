eb793e7435f536ce6c4ea1934d77567541045163
yikenyiken <youssf.iken@gmail.com>
2026-02-13 10:55:42 +0100

    feat<sidebar>: add message sending in Chat

--- frontend/src/components/layout/Sidebar/Chat/Footer/ChatFooter.tsx
+++ frontend/src/components/layout/Sidebar/Chat/Footer/ChatFooter.tsx
@@ -1,10 +1,22 @@
+import { useChatStore } from '@stores/chat.store';
 import InputField from '@ui/InputField';
+import { useState } from 'react';
 
 function ChatFooter() {
+  const [message, setMessage] = useState('');
+  const setInputValue = (value: string) => setMessage(value);
+  const addMessage = useChatStore((state) => state.addMessage);
+
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    message && addMessage(message);
+    setMessage('');
+  };
+
   return (
     <div className="p-4 flex-shrink-0 border-t border-white/10">
-      <div className="flex items-center gap-2">
-        <InputField placeholder="Type a message..." />
+      <form className="flex items-center gap-2" onSubmit={sendMessage}>
+        <InputField placeholder="Type a message..." value={message} setInputVal={setInputValue}/>
         <button
           className={[
             'text-sm font-bold text-white bg-primary',
@@ -14,7 +26,7 @@ function ChatFooter() {
         >
           Send
         </button>
-      </div>
+      </form>
     </div>
   );
 }
