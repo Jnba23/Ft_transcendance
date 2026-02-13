c94a71ce649bb074c95e3c50661c3d041e38a798
yikenyiken <youssf.iken@gmail.com>
2026-02-13 11:28:57 +0100

    feat<sidebar>: add View Profile option behavior

--- frontend/src/components/layout/Sidebar/Chat/Header/ChatOptions.tsx
+++ frontend/src/components/layout/Sidebar/Chat/Header/ChatOptions.tsx
@@ -2,16 +2,24 @@ import { useRef } from 'react';
 import getTransitionClasses from '@utils/transitionStyles';
 import ChatOptsItem from './ChatOptsItem';
 import useClickOutside from '@hooks/useClickOutside';
+import { useNavigate } from 'react-router-dom';
 
 type ChatOptionsProps = {
   isOpen: boolean;
+  hide: () => void;
+  hideChat: () => void
+  user_id: number,
   isFriend?: boolean;
   btnRef: React.RefObject<HTMLButtonElement | null>;
-  hide: () => void;
 };
 
-function ChatOptions({ isOpen, isFriend, btnRef, hide }: ChatOptionsProps) {
+function ChatOptions({ isOpen, hide, hideChat,user_id, isFriend, btnRef }: ChatOptionsProps) {
   const optsRef = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
+  const toProfile = () => {
+    hideChat();
+    navigate(`/profile/${user_id}`);
+  }
 
   useClickOutside(isOpen, hide, [optsRef, btnRef]);
 
@@ -25,9 +33,7 @@ function ChatOptions({ isOpen, isFriend, btnRef, hide }: ChatOptionsProps) {
       ].join(' ')}
       ref={optsRef}
     >
-      <ChatOptsItem icon="visibility" label="View Profile" />
-      <ChatOptsItem icon="block" label="Block User" />
-      <ChatOptsItem icon="sports_esports" label="Send Game Request" />
+      <ChatOptsItem icon="visibility" label="View Profile" onClick={toProfile}/>
       {isFriend && (
         <ChatOptsItem icon="sports_esports" label="Send Game Request" />
       )}
