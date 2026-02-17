400af0892c508b888b55278e6fac6d310fa37da0
yikenyiken <youssf.iken@gmail.com>
2026-02-17 16:39:08 +0000

    feat<app>: add Direct Messages Store state controllers

--- frontend/src/stores/directMessages.store.ts
+++ frontend/src/stores/directMessages.store.ts
@@ -8,10 +8,14 @@ interface DirectMessagesState {
 	initialze: () => void,
 
 	addConversation: (convo: Conversation) => void,
-	removeConversation: (id: number) => void
+	removeConversation: (id: number) => void,
+	getConversationById: (id: number) => Conversation | undefined,
+	getConversationByUserId: (userId: number) => Conversation | undefined,
+	replaceConversation: (updated: Conversation) => void,
+	incrementUnread: (convo_id: number) => void
 }
 
-export const useDirectMessagesStore = create<DirectMessagesState>((set) => ({
+export const useDirectMessagesStore = create<DirectMessagesState>((set, get) => ({
 	conversations: [],
 
 	initialze: async () => {
@@ -31,5 +35,36 @@ export const useDirectMessagesStore = create<DirectMessagesState>((set) => ({
 		set((state) => ({
 			conversations: state.conversations.filter(c => c.id != id)
 		}))
-	}
+	},
+
+	getConversationById: (id) => {
+		const conversations = get().conversations;
+		const result = conversations.find(c => c.id === id);
+
+		return result;
+	},
+
+	getConversationByUserId: (userId) => {
+		const conversations = get().conversations;
+		const result = conversations.find(c => c.user.id == userId);
+
+		return result;
+	},
+
+	replaceConversation: (updated) => {
+		set((state) => ({
+			conversations: state.conversations.map(c => 
+				c.id == updated.id ? updated : c
+			)
+		}));
+	},
+
+	incrementUnread: (convo_id) => {
+		let convo = get().getConversationById(convo_id);
+
+		convo!.unread_count += 1;
+
+		get().removeConversation(convo_id);
+		get().addConversation(convo!);
+	},
 }));
