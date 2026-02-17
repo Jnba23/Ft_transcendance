88d7b4383940ad16e8d75766dfd0693c9ba368ed
yikenyiken <youssf.iken@gmail.com>
2026-02-17 16:03:01 +0000

    feat<sidebar>: add placeholder for emtpy DM and non-existing user search

--- frontend/src/components/layout/Sidebar/sections/DirectMessages/DMList.tsx
+++ frontend/src/components/layout/Sidebar/sections/DirectMessages/DMList.tsx
@@ -1,14 +1,28 @@
 import { Conversation } from "types/conversation";
 import DMListItem from "./DMListItem";
 import { useState } from "react";
+import NoMatchedUser from "../shared/NoMatchedUser";
+import NoConversations from "./NoConversations";
 
 type DMListProps = {
 	conversations: Conversation[],
-	openChat: () => void
+	isSearching: boolean
 }
 
-function DMList({ conversations, openChat }: DMListProps) {
+function DMList({ conversations, isSearching }: DMListProps) {
 	const [openItemId, setOpenItemId] = useState< number| null>(null);
+
+	if (!conversations.length && !isSearching) {
+		return (
+			<NoConversations />
+		);
+	}
+	
+	if (!conversations.length) {
+		return (
+			<NoMatchedUser />
+		);
+	}
 
 	return (
 		<div className="flex flex-col gap-1 mt-2 ">
@@ -17,7 +31,6 @@ function DMList({ conversations, openChat }: DMListProps) {
 					return <DMListItem
 								key={convo.id}
 								convo={convo}
-								openChat={openChat}
 								hasOpenOpts={openItemId === convo.id}
 								openOptions={() =>
 									setOpenItemId(prev => (prev === convo.id ? null : convo.id))
