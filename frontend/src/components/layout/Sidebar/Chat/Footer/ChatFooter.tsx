c7f51861c1bc88f3300aa00fe7ffdccee82091ff
yikenyiken <youssf.iken@gmail.com>
2026-02-17 15:57:31 +0000

    feat<sidebar>: add Send Message logic in ChatFooter

--- frontend/src/components/layout/Sidebar/Chat/Footer/ChatFooter.tsx
+++ frontend/src/components/layout/Sidebar/Chat/Footer/ChatFooter.tsx
@@ -1,22 +1,32 @@
 import { useChatStore } from '@stores/chat.store';
+import { useDirectMessagesStore } from '@stores/directMessages.store';
 import InputField from '@ui/InputField';
-import { useState } from 'react';
 
-function ChatFooter() {
-  const [message, setMessage] = useState('');
-  const setInputValue = (value: string) => setMessage(value);
+type ChatFooterProps = {
+  inputValue: string
+  setInputValue: React.Dispatch<React.SetStateAction<string>>,
+}
+
+function ChatFooter({ inputValue, setInputValue }: ChatFooterProps) {
   const addMessage = useChatStore((state) => state.addMessage);
+  const addConversation = useDirectMessagesStore((state) => state.addConversation);
 
-  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+  const sendMessage =  async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    message && addMessage(message);
-    setMessage('');
+
+    if (inputValue) {
+      const result = await addMessage(inputValue);
+
+      if (result.isNewConversation) addConversation(result.conversation);
+
+      setInputValue('');
+    }
   };
 
   return (
     <div className="p-4 flex-shrink-0 border-t border-white/10">
       <form className="flex items-center gap-2" onSubmit={sendMessage}>
-        <InputField placeholder="Type a message..." value={message} setInputVal={setInputValue}/>
+        <InputField placeholder="Type a message..." value={inputValue} setInputVal={setInputValue}/>
         <button
           className={[
             'text-sm font-bold text-white bg-primary',
