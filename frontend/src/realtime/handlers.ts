931bd961afdd5a95551b6dd77957eb9ba06c1983
yikenyiken <youssf.iken@gmail.com>
2026-02-18 15:48:13 +0000

    chore: add socket event handlers and wiring

---
+++ frontend/src/realtime/handlers.ts
@@ -0,0 +1,55 @@
+import { Message } from "types/message"
+import { useChatStore } from "@stores/chat.store"
+import { useDirectMessagesStore } from "@stores/directMessages.store"
+import { chatApi } from "@api/chat.api"
+import { useAuth } from "@context/AuthContext"
+import { UserSummaryRes } from "@api/user.api"
+import { Conversation } from "types/conversation"
+
+interface newMessagePayload {
+	conversation_id: number,
+	message: Message
+}
+
+interface newConversationPayload {
+	conversation_id: number,
+	user: UserSummaryRes
+}
+
+export const handleNewMessage = async (payload: newMessagePayload) => {
+	const chatStore = useChatStore.getState();
+	const dmStore = useDirectMessagesStore.getState();
+	const {user: me} = useAuth();
+
+	const isSentByMe = payload.message.sender_id === me?.id;
+	const isConvoOpen = chatStore.isOpen &&
+		payload.conversation_id === chatStore.conversation_id;
+
+	if (isConvoOpen) {
+		chatStore.appendMessage(payload.message);
+
+		if (!isSentByMe)
+			await chatApi.markConversationRead(payload.conversation_id);
+
+	} else if (!isSentByMe) { // convo closed
+		dmStore.incrementUnread(payload.conversation_id);
+	}
+}
+
+export const handleNewConversation = (payload: newConversationPayload) => {
+	const dmStore = useDirectMessagesStore.getState();
+	const chatstore = useChatStore.getState();
+
+	const isConvoOpen = chatstore.isOpen && payload.user.id === chatstore.user?.id;
+	const newConvo: Conversation = {
+		id: payload.conversation_id,
+		user: payload.user,
+		unread_count: 0
+	}
+
+	dmStore.addConversation(newConvo);
+
+	if (isConvoOpen) {
+		chatstore.setConversationId(newConvo.id);
+	}
+};
