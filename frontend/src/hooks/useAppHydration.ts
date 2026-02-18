8d12c25b90b7fe7ea95cc25b065043c3252921a5
yikenyiken <youssf.iken@gmail.com>
2026-02-18 15:50:42 +0000

    refactor<app>: wire events using registerSocketEvents in useAppHydration hook

--- frontend/src/hooks/useAppHydration.ts
+++ frontend/src/hooks/useAppHydration.ts
@@ -3,12 +3,13 @@ import { useUserDirectoryStore } from "@stores/userDirectory.store";
 import { useFriendRequestsStore } from '@stores/friendRequests.store';
 import { useDirectMessagesStore } from "@stores/directMessages.store"
 import { getSocket } from '@services/socket';
-import { newMessagePayload } from 'types/newMessagePayload';
-import { markAsRead, useChatStore } from '@stores/chat.store';
-import { newConversationPayload } from 'types/newConversationPayload';
-import { Conversation } from 'types/conversation';
+import { useAuth } from '@context/AuthContext';
+import { registerSocketEvents } from 'realtime/register';
 
 function useAppHydration() {
+	const { user: me } = useAuth();
+	const currentUserId = me?.id;
+
 	const intializeUserDirectory = useUserDirectoryStore(
 		(state) => state.initialize
 	);
@@ -26,69 +27,12 @@ function useAppHydration() {
 	}, [intializeUserDirectory, initializeFriendRequests, initializeDirectMessages]);
 
 	useEffect(() => {
+		if (!currentUserId) return;
+
 		const socket = getSocket();
-		socket.emit('register', 1);
-	});
-
-	useEffect(() => {
-		const socket = getSocket();
-
-		const handleNewMessage = async (payload: newMessagePayload) => {
-			console.log("new message received!");
-
-			const chatStore = useChatStore.getState();
-			const dmStore = useDirectMessagesStore.getState();
-
-			const isActive = chatStore.isOpen &&
-				payload.conversation_id === chatStore.conversation_id;
-
-			if (isActive) {
-				chatStore.appendMessage(payload.message);
-				if (payload.message.sender_id !== 1)
-					await markAsRead(payload.conversation_id);
-			} else if (payload.message.sender_id !== 1) {
-				dmStore.incrementUnread(payload.conversation_id);
-			}
-		}
-
-		socket.on('newMessage', handleNewMessage);
-
-		return () => {
-			socket.off('newMessage', handleNewMessage);
-		};
-	}, []);
-
-	useEffect(() => {
-		const socket = getSocket();
-
-		const handleNewConversation = (payload: newConversationPayload) => {
-			const dmStore = useDirectMessagesStore.getState();
-			const chatstore = useChatStore.getState();
-
-			const newConvo: Conversation = {
-				id: payload.conversation_id,
-				user: payload.user,
-				unread_count: 0
-			}
-
-			dmStore.addConversation(newConvo);
-
-			// to let the user create their own conversation using this event
-			//
-			// we want to set the received convo id to the open chat
-			//  if it is open with the user we created the convo with
-
-			if (chatstore.isOpen && newConvo.user.id === chatstore.user?.id) {
-				chatstore.setConversationId(newConvo.id);
-			}
-		};
-
-		socket.on('newConversation', handleNewConversation);
-
-		return () => {
-			socket.off('newConversation', handleNewConversation);
-		};
-	}, []);
+		socket.emit('register', currentUserId);
+		registerSocketEvents();
+	}, [currentUserId]);
 }
 
 export default useAppHydration;
