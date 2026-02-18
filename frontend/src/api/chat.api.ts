c468b5d432db80dab8dc8365c27ab2b0ec72b4d3
yikenyiken <youssf.iken@gmail.com>
2026-02-18 11:31:58 +0000

    feat<app>: add markConversationRead to chat.api

--- frontend/src/api/chat.api.ts
+++ frontend/src/api/chat.api.ts
@@ -28,8 +28,11 @@ interface GetMessagesRes {
 	messages: Message[]
 }
 
+interface MarkConversationReadRes {
+	status: boolean
+}
+
 export const chatApi = {
-	// 
 	createConversation: async (data: CreateConversationReq) => {
 		const response = await client.post<CreateConversationRes>('/conversations', data); // add path
 		return response.data;
@@ -47,6 +50,11 @@ export const chatApi = {
 
 	getMessages: async (conversation_id: number) => {
 		const response = await client.get<GetMessagesRes>(`/conversations/${conversation_id}/messages`);
+		return response.data;
+	},
+
+	markConversationRead: async (conversation_id: number) => {
+		const response = await client.post<MarkConversationReadRes>(`/conversations/${conversation_id}/read`);
 		return response.data;
 	}
 }
