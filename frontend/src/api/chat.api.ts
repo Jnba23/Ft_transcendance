12d62117b8b0df91eaee25bdd28f3443f42d0213
yikenyiken <youssf.iken@gmail.com>
2026-02-18 00:56:29 +0000

    chore: add Chat API

---
+++ frontend/src/api/chat.api.ts
@@ -0,0 +1,52 @@
+import { Conversation } from "types/conversation";
+import { client } from "./client";
+import { Message } from "types/message"
+
+interface CreateConversationReq {
+	other_id: number
+}
+
+interface CreateConversationRes {
+	conversation_id: number
+}
+
+interface CreateMessageReq {
+	conversation_id: number,
+	content: string
+}
+
+interface CreateMessageRes {
+	message_id: number,
+	sent_at: string
+}
+
+interface GetConversationsRes {
+	conversations: Conversation[]
+}
+
+interface GetMessagesRes {
+	messages: Message[]
+}
+
+export const chatApi = {
+	// 
+	createConversation: async (data: CreateConversationReq) => {
+		const response = await client.post<CreateConversationRes>('/conversations', data); // add path
+		return response.data;
+	},
+
+	createMessage: async (data: CreateMessageReq) => {
+		const response = await client.post<CreateMessageRes>('/messages', data);
+		return response.data;
+	},
+
+	getConversations: async () => {
+		const response = await client.get<GetConversationsRes>('/conversations');
+		return response.data;
+	},
+
+	getMessages: async (conversation_id: number) => {
+		const response = await client.get<GetMessagesRes>(`/conversations/${conversation_id}/messages`);
+		return response.data;
+	}
+}
