9f1e22614ca3df8fc40c29ec27293cd7b631e5ca
yikenyiken <youssf.iken@gmail.com>
2026-02-23 06:38:42 +0000

    setup handler for /chat namespace with auth middleware

--- backend/src/chat/socketHandler.ts
+++ backend/src/chat/socketHandler.ts
@@ -1,3 +1,16 @@
 import { Server } from 'socket.io';
+import { socketAuthMiddleware } from '../middleware/socketAuthMiddleware.js';
 
-export const setupChatHandler = (io: Server) => {};
+export const setupChatHandler = (io: Server) => {
+	const chatNs = io.of('/chat');
+
+	chatNs.use(socketAuthMiddleware);
+
+	chatNs.on('connection', (socket) => {
+		socket.join(`user_${socket.data.userId}`);
+
+		socket.on('disconnect', () => {
+			console.log('Socket disconnected: ', socket.id);
+		})
+	});
+};
