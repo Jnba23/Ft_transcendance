a6cff39da59cc95a91d413be07c417ad17921aa9
yikenyiken <youssf.iken@gmail.com>
2026-02-23 06:40:04 +0000

    feat<chat>: add necessary types

---
+++ backend/src/chat/types.ts
@@ -0,0 +1,24 @@
+export interface ConversationRow {
+	user_id_1: number,
+	user_id_2: number,
+}
+
+export interface MessageRow {
+	id: number,
+	conversation_id: number,
+	sender_id: number,
+	content: string,
+	sent_at: string
+}
+
+export interface ConversationListRow {
+	conversation_id: number,
+	// User part
+	user_id: number,
+	username: string,
+	avatar_url: string,
+	level: number,
+	status: string
+	//
+	unread_count: number
+}
