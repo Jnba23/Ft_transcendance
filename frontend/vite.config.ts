a1aa1bbe3e0459a02526015369f3c64f24d29d7d
yikenyiken <youssf.iken@gmail.com>
2026-02-23 06:25:27 +0000

    chore: add @style path alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
       '@utils': path.resolve(__dirname, 'src/utils'),
       '@assets': path.resolve(__dirname, 'src/assets'),
