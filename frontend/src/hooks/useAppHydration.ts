cbc514f24e4a8052ffc084b74a2a16be8c12e8ec
yikenyiken <youssf.iken@gmail.com>
2026-03-02 21:40:14 +0000

    feat<app>: initialize friends/friends_request stores and register related socket.io events

--- frontend/src/hooks/useAppHydration.ts
+++ frontend/src/hooks/useAppHydration.ts
@@ -2,13 +2,17 @@ import { useEffect } from 'react';
 import { useUserDirectoryStore } from "@stores/userDirectory.store";
 import { useFriendRequestsStore } from '@stores/friendRequests.store';
 import { useDirectMessagesStore } from "@stores/directMessages.store"
-import { getSocket } from '@services/chat/socket';
+import { createChatSocket } from '@services/chat/socket';
 import { useAuth } from '@context/AuthContext';
-import { handleNewConversation, handleNewMessage } from '@realtime/handlers';
+import { handleNewConversation, handleNewMessage } from '@realtime/chat/chat.handlers';
+import { createFriendsSocket } from '@services/friends/socket';
+import { handleNewFriendRequest, handleUpdateFriendRequest } from '@realtime/friends/friends.handlers';
+import { useFriendsStore } from '@stores/Friends.store';
+import { createUsersSocket } from '@services/users/socket';
+import { handleUpdateOnlineStatus } from '@realtime/users/users.handlers';
 
 function useAppHydration() {
 	const { user: me } = useAuth();
-	const currentUserId = me?.id;
 
 	const intializeUserDirectory = useUserDirectoryStore(
 		(state) => state.initialize
@@ -17,20 +21,31 @@ function useAppHydration() {
 		(state) => state.initialze
 	);
 	const initializeFriendRequests = useFriendRequestsStore(
-    	(state) => state.initialize
-  	);
+		(state) => state.initialize
+	);
+	const initializeFriends = useFriendsStore(
+		(state) => state.initialize
+	);
 
 	useEffect(() => {
+		if (!me) return;
+
 		intializeUserDirectory(me);
 		initializeDirectMessages();
 		initializeFriendRequests();
-	}, [intializeUserDirectory, initializeFriendRequests, initializeDirectMessages]);
+		initializeFriends();
+	}, [
+		intializeUserDirectory,
+		initializeFriendRequests,
+		initializeDirectMessages,
+		initializeFriends,
+		me
+	]);
 
-	useEffect(() => {
-		if (!currentUserId) return;
+	useEffect(() => { // prevent not logged user socket connection
+		if (!me) return;
 
-		const socket = getSocket();
-		socket.emit('register', currentUserId);
+		const socket = createChatSocket();
 
 		// register socket events
 		socket.on('newConversation', handleNewConversation);
@@ -40,7 +55,35 @@ function useAppHydration() {
 			socket.off('newConversation', handleNewConversation);
 			socket.off('newMessage', handleNewMessage);
 		};
-	}, [currentUserId]);
+	}, [me]);
+
+	useEffect(() => {
+		if (!me) return;
+
+		const socket = createFriendsSocket();
+
+		// register socket events
+		socket.on('newFriendRequest', handleNewFriendRequest);
+		socket.on('updateFriendRequest', handleUpdateFriendRequest);
+
+		return () => {
+			socket.off('newFriendRequest', handleNewFriendRequest);
+			socket.off('updateFriendRequest', handleUpdateFriendRequest);
+		};
+	}, [me]);
+
+	useEffect(() => {
+		if (!me) return;
+
+		const socket = createUsersSocket();
+
+		// register socket events
+		socket.on('updateOnlineStatus', handleUpdateOnlineStatus);
+
+		return () => {
+			socket.off('updateOnlineStatus', handleUpdateOnlineStatus);
+		};
+	}, [me]);
 }
 
 export default useAppHydration;
