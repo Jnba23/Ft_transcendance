efb0f2134c49a1f80243c3f88d8c9965f9d021f0
yikenyiken <youssf.iken@gmail.com>
2026-03-02 20:55:06 +0000

    refactor<navbar>: remove notifications

--- frontend/src/components/layout/Navbar/Navbar.tsx
+++ frontend/src/components/layout/Navbar/Navbar.tsx
@@ -1,19 +1,9 @@
 import { useRef, useState } from 'react';
-import NotificationsBtn from './Notifications/NotificationsBtn';
-import NotificationsMenu from './Notifications/NotificationsMenu';
 import UserMenuBtn from './UserMenu/UserMenuBtn';
 import UserMenu from './UserMenu/UserMenu';
 
 function Navbar() {
-  const [isNotifsOpen, setIsNotifsOpen] = useState(false);
   const [isUserOpen, setIsUserOpen] = useState(false);
-
-  const notifsMenu = {
-    isOpen: isNotifsOpen,
-    toggle: () => setIsNotifsOpen(!isNotifsOpen),
-    hide: () => setIsNotifsOpen(false),
-    buttonRef: useRef<HTMLButtonElement>(null),
-  };
 
   const userMenu = {
     isOpen: isUserOpen,
@@ -24,14 +14,6 @@ function Navbar() {
 
   return (
     <header className="flex gap-3 items-center justify-end mb-8">
-      <div className="relative">
-        <NotificationsBtn
-          onClick={notifsMenu.toggle}
-          buttonRef={notifsMenu.buttonRef}
-        />
-        <NotificationsMenu {...notifsMenu} />
-      </div>
-
       <div className="relative">
         <UserMenuBtn
           isOpen={userMenu.isOpen}
