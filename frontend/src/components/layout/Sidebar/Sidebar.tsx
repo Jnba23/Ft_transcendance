6289c2530967d4e525a58ad46cc5e9ba1d4d3687
yikenyiken <youssf.iken@gmail.com>
2026-03-04 21:10:36 +0000

    feat<sidebar>: add sidebar closing behavior and button for small screens

--- frontend/src/components/layout/Sidebar/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar/Sidebar.tsx
@@ -2,22 +2,31 @@ import Navigation from './Navigation/Navigation';
 import DirectMessages from './sections/DirectMessages/DirectMessages';
 import UserDirectory from './sections/UserDirectory/UserDirectory';
 import Chat from './Chat/Chat';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import getTransitionClasses from '@utils/transitionStyles';
+import useClickOutside from '@hooks/useClickOutside';
+import { useLayoutStore } from '@stores/layout.store';
 
 function Sidebar() {
+  const ref = useRef<HTMLElement>(null);
+  const {isSidebarShown, hideSidebar} = useLayoutStore((state) => state);
   const [isDMOpen, setIsDMOpen] = useState(true);
   const switchSection = () => setIsDMOpen(!isDMOpen);
+
+  useClickOutside(isSidebarShown, hideSidebar, [ref]);
 
   return (
     <aside
       className={[
-        'w-64 p-4',
-        'bg-[#16213E]/30',
+        `${getTransitionClasses(isSidebarShown, 'sidebar')}`,
+        'w-64 p-4 md:block',
+        'bg-[#16213E] md:bg-[#16213E]/30',
         'border-r border-white/10',
-      ].join(' ')}
-    >
+        'fixed inset-y-0 left-0 z-60',
+        'md:static md:translate-x-0',
+    ].join(' ')} ref={ref}>
       <div className="flex flex-col gap-8 h-full">
-        <Navigation />
+        <Navigation/>
         <div className="overflow-hidden min-h-0">
           <div
             className={[
