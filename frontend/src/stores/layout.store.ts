f42dfacb3a27791bf7a781f5f1a4a84a31105fb0
yikenyiken <youssf.iken@gmail.com>
2026-03-04 21:19:25 +0000

    feat<app>: add layout store

---
+++ frontend/src/stores/layout.store.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+
+interface layoutStore {
+	isSidebarShown: boolean,
+	showSidebar: () => void,
+	hideSidebar: () => void,
+}
+
+export const useLayoutStore = create<layoutStore>((set) => ({
+	isSidebarShown: false,
+
+	showSidebar: () => {
+		set({ isSidebarShown: true });
+	},
+	hideSidebar: () => {
+		set({ isSidebarShown: false });
+	}
+}));
