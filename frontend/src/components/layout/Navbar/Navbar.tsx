5ff8eb757540f4137ffbe26654a7b5b40da24da5
yikenyiken <youssf.iken@gmail.com>
2026-03-04 21:08:02 +0000

    feat<navbar>: add small screen styles and sidebar button

--- frontend/src/components/layout/Navbar/Navbar.tsx
+++ frontend/src/components/layout/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react';
 import UserMenuBtn from './UserMenu/UserMenuBtn';
 import UserMenu from './UserMenu/UserMenu';
+import SidebarMenu from './SidebarMenu/SidebarMenu';
 
 function Navbar() {
   const [isUserOpen, setIsUserOpen] = useState(false);
@@ -13,7 +14,11 @@ function Navbar() {
   };
 
   return (
-    <header className="flex gap-3 items-center justify-end mb-8">
+    <header className={[
+      "flex gap-3 items-center justify-between",
+      "md:justify-end mb-8"
+    ].join(' ')}>
+      <SidebarMenu />
       <div className="relative">
         <UserMenuBtn
           isOpen={userMenu.isOpen}
---
+++ frontend/src/components/layout/Navbar/SidebarMenu/SidebarMenu.tsx
@@ -0,0 +1,20 @@
+import { useLayoutStore } from "@stores/layout.store";
+
+function SidebarMenu() {
+	const showSidebar = useLayoutStore((state) => state.showSidebar);
+
+	return (
+		<button className={[
+			'flex items-center justify-center size-10',
+			'bg-[#16213E]/50 border border-white/10 rounded-lg',
+			'hover:bg-white/10 transition-colors text-white/80',
+			'md:hidden'
+			].join(' ')} onClick={showSidebar}>
+			<span className='material-symbols-outlined'>
+				menu
+			</span>
+		</button>
+	);
+}
+
+export default SidebarMenu;
