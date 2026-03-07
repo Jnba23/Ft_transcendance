0ce9c562cb61590766abbedf0740cce00de9c9b7
yikenyiken <youssf.iken@gmail.com>
2026-03-07 15:42:59 +0000

    feat<ProfileBadge>: replace mock info with fetched info

--- frontend/src/pages/profile/ProfileBadge/ProfileBadge.tsx
+++ frontend/src/pages/profile/ProfileBadge/ProfileBadge.tsx
@@ -1,14 +1,28 @@
-import boy from '@assets/boy.jpg';
 import Avatar from '@components/ui/Avatar';
 
-function ProfileBadge() {
+type ProfileBadgeProps = {
+  userId: number,
+  username: string,
+  level: number,
+  created_at: string,
+}
+
+function ProfileBadge({ userId, username, level, created_at }: ProfileBadgeProps) {
+  const date = new Date(created_at.split(' ')[0]);
+  const month = date.toLocaleString("en-US", { month: "long" })
+  const year = date.toLocaleString("en-US", { year: "numeric" });
+  const joined_at = `${month} ${year}`;
+
   return (
-    <div className="flex items-center gap-6">
+    <div className={[
+      "flex items-center gap-6 flex-col md:flex-row",
+      "text-center md:text-start"
+    ].join(' ')}>
       <div className="relative">
         {' '}
         {/* avatar */}
         <div className="border-4 border-primary rounded-full">
-          <Avatar path={boy} section="profile" />
+          <Avatar userId={userId} section="profile" />
         </div>
         <div
           className={[
@@ -16,8 +30,14 @@ function ProfileBadge() {
             '-bottom-1 -right-1 px-2 py-0.5 border border-white/10',
           ].join(' ')}
         >
-          <span className={['text-xs', 'font-bold text-yellow-400'].join(' ')}>
-            Lv.24
+          <span className={[
+            'text-xs flex items-center gap-1',
+            'font-bold text-yellow-400'
+          ].join(' ')}>
+            <span className='material-symbols-outlined !text-[14px]'>
+              trophy
+            </span>
+              Rating {level}
           </span>
         </div>
       </div>
@@ -25,9 +45,9 @@ function ProfileBadge() {
         {' '}
         {/*text */}
         <h1 className="text-3xl font-bold tracking-right text-[#EFEFEF]">
-          Alex_Gamer99
+          {username}
         </h1>
-        <p className="text-white/60 mt-1">Joined: July 2024</p>
+        <p className="text-white/60 mt-1">Joined: {joined_at}</p>
       </div>
     </div>
   );
