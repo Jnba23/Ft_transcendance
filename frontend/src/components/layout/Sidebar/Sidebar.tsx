9622f0c8f31c831ca7a8d64cee0a5553c4da795e
yikenyiken <youssf.iken@gmail.com>
2026-03-07 15:17:16 +0000

    feat<sidear>: hide Sidebar while in match-making or in game

--- frontend/src/components/layout/Sidebar/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar/Sidebar.tsx
@@ -9,39 +9,42 @@ import { useLayoutStore } from '@stores/layout.store';
 
 function Sidebar() {
   const ref = useRef<HTMLElement>(null);
-  const {isSidebarShown, hideSidebar} = useLayoutStore((state) => state);
+  const {isSidebarShown, hideSidebar, isSidebarOmited} = useLayoutStore((state) => state);
   const [isDMOpen, setIsDMOpen] = useState(true);
   const switchSection = () => setIsDMOpen(!isDMOpen);
 
   useClickOutside(isSidebarShown, hideSidebar, [ref]);
 
   return (
-    <aside
-      className={[
-        `${getTransitionClasses(isSidebarShown, 'sidebar')}`,
-        'w-64 p-4 md:block',
-        'bg-[#16213E] md:bg-[#16213E]/30',
-        'border-r border-white/10',
-        'fixed inset-y-0 left-0 z-60',
-        'md:static md:translate-x-0',
-    ].join(' ')} ref={ref}>
-      <div className="flex flex-col gap-8 h-full">
-        <Navigation/>
-        <div className="overflow-hidden min-h-0">
-          <div
-            className={[
-              `${!isDMOpen && 'transform-[translateX(-100%)]'}`,
-              'flex transition-[transform] duration-300',
-              'h-full',
-            ].join(' ')}
-          >
-            <DirectMessages switchSection={switchSection} />
-            <UserDirectory switchSection={switchSection} />
+    <>
+      <aside
+        className={[
+          `${isSidebarOmited ? 'md:w-0 md:hidden' : ''}`,
+          `${getTransitionClasses(isSidebarShown, 'sidebar')}`,
+          'w-64 p-4 md:block',
+          'bg-[#16213E] md:bg-[#16213E]/30',
+          'border-r border-white/10',
+          'fixed inset-y-0 left-0 z-60',
+          'md:static md:translate-x-0',
+      ].join(' ')} ref={ref}>
+        <div className="flex flex-col gap-8 h-full">
+          <Navigation/>
+          <div className="overflow-hidden min-h-0">
+            <div
+              className={[
+                `${!isDMOpen && 'transform-[translateX(-100%)]'}`,
+                'flex transition-[transform] duration-300',
+                'h-full',
+              ].join(' ')}
+            >
+              <DirectMessages switchSection={switchSection} />
+              <UserDirectory switchSection={switchSection} />
+            </div>
           </div>
         </div>
-      </div>
+      </aside>
       <Chat />
-    </aside>
+    </>
   );
 }
 
