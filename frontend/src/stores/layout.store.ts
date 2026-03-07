c70e2b7bdcd8ab7b7ecd3f22398085b81f146906
yikenyiken <youssf.iken@gmail.com>
2026-03-07 15:45:19 +0000

    feat<layout>: add navbar/sidebar visibility contollers

--- frontend/src/stores/layout.store.ts
+++ frontend/src/stores/layout.store.ts
@@ -4,6 +4,15 @@ interface layoutStore {
 	isSidebarShown: boolean,
 	showSidebar: () => void,
 	hideSidebar: () => void,
+
+	isNavbarShown: boolean,
+	showNavbar: () => void,
+	hideNavbar: () => void,
+
+	isSidebarOmited: boolean,
+
+	omitSidebar: () => void,
+	unomitSidebar: () => void
 }
 
 export const useLayoutStore = create<layoutStore>((set) => ({
@@ -14,5 +23,24 @@ export const useLayoutStore = create<layoutStore>((set) => ({
 	},
 	hideSidebar: () => {
 		set({ isSidebarShown: false });
+	},
+
+	isNavbarShown: true,
+	
+	showNavbar: () => {
+		set({ isNavbarShown: true });
+	},
+	hideNavbar: () => {
+		set({ isNavbarShown: false });
+	},
+
+	isSidebarOmited: false,
+
+	omitSidebar: () => {
+		set({ isSidebarOmited: true });
+	},
+
+	unomitSidebar: () => {
+		set({ isSidebarOmited: false });
 	}
 }));
