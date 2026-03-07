7e8e8fb5c13f54961359f9408a955f7577b72d02
yikenyiken <youssf.iken@gmail.com>
2026-03-07 15:41:14 +0000

    feat<profile>: integrate game stats and game history

---
+++ frontend/src/pages/profile/GameInsights/GameHistory/GameHistory.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import HistoryHeader from "./HistoryHeader";
+import { GameHistoryItem } from "types/gameHistory";
+import { useErrorStore } from "@stores/error.store";
+import { GameHistoryData, gamesAPI } from "@api/games.api";
+import GameHistoryUnit from "./GameHistoryUnit";
+
+type GameHistoryProps = {
+	userId: number;
+}
+
+function GameHistory({ userId }: GameHistoryProps) {
+	const INITIAL_PAGE = 1;
+	const LIMIT = 10;
+	const showError = useErrorStore((state) => state.showError);
+	const [gameType, setGameType] = useState('pong');
+	const [page, setPage] = useState(INITIAL_PAGE);
+	const [data, setData] = useState<GameHistoryData | null>(null);
+	const [games, setGames] = useState<GameHistoryItem[]>([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		if (!userId) return;
+
+		const fetchGames = async () => {
+			try {
+				const response = await gamesAPI.getGameHistory(userId, page, LIMIT, gameType);
+				const games = response.data.games;
+				setGames(games.filter(game => game.game_type === gameType));
+				setData(response.data);
+			} catch {
+				showError('Failed to fetch game history');
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchGames();
+	}, [gameType, page, userId]);
+
+	if (page > data!?.totalPages) setPage(data!?.totalPages); // reset to total pages after profile switch
+
+	if (loading) return <div></div>
+
+	return (
+		<div className="bg-[#16213E]/50 rounded-xl p-6 border border-white/10">
+			<HistoryHeader
+				setPage={setPage}
+				setGameType={setGameType}
+				page={data!.page ?? 1}
+				totalPages={data!.totalPages ?? 1}
+			/>
+			<div className="max-h-64 h-64 overflow-y-auto custom-scrollbar pr-2 flex flex-col">
+				<div className="flex flex-col gap-2">
+					{
+						games.map((game) => {
+							return <GameHistoryUnit key={game.id} game={game} userId={game.opponent_id}/>
+						})
+					}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default GameHistory;
---
+++ frontend/src/pages/profile/GameInsights/GameHistory/HistoryHeader.tsx
@@ -0,0 +1,82 @@
+import TwoOptionsToggle from "@components/ui/TwoOptionsToggle";
+
+type HistoryHeaderProps = {
+	setPage: React.Dispatch<React.SetStateAction<number>>;
+	setGameType: React.Dispatch<React.SetStateAction<string>>;
+	page: number;
+	totalPages: number;
+}
+
+function HistoryHeader({ setPage, setGameType, page, totalPages }: HistoryHeaderProps) {
+	const INITIAL_PAGE = 1;
+
+	
+	return (
+		<div>
+			<div className="flex flex-wrap justify-between items-center gap-4 mb-4">
+				<h3 className="text-white text-lg font-bold leading-tight tracking-[-0.015em]">
+					Game History
+				</h3>
+				<div className="flex items-center gap-4">
+					{/* prev / next */}
+					<div className="flex gap-2 items-center">
+						<button className={[
+							"flex items-center justify-center p-2 text-white/70",
+							"hover:bg-white/10 hover:text-white transition-colors",
+							"rounded-lg border border-white/10"
+						].join(' ')} onClick={() => setPage(prev => (prev - 1) > 1 ? prev - 1 : 1)}>
+							<span className="material-symbols-outlined !text-base">
+								chevron_left
+							</span>
+							<span className="text-sm font-medium pr-1">
+								Previous
+							</span>
+						</button>
+						<span className="text-sm font-medium text-white/70 px-1">
+							{page} / {totalPages || 1}
+						</span>
+						<button className={[
+							"flex items-center justify-center p-2 text-white/70",
+							"hover:bg-white/10 hover:text-white transition-colors",
+							"rounded-lg border border-white/10"
+						].join(' ')} onClick={() => setPage(prev => (prev + 1) < totalPages ? prev + 1 : totalPages)}>
+							<span className="text-sm font-medium pr-1">
+								Next
+							</span>
+							<span className="material-symbols-outlined !text-base">
+								chevron_right
+							</span>
+						</button>
+					</div>
+					{/* two options toggle */}
+					<TwoOptionsToggle
+						opt1="Pong History"
+						opt2="RPS History"
+						section="profile"
+						onOpt1Select={() => {setGameType('pong'); setPage(INITIAL_PAGE)}}
+						onOpt2Select={() => {setGameType('RPS'); setPage(INITIAL_PAGE)}}
+					/>
+				</div>
+			</div>
+			<div className={[
+				"grid grid-cols-4 gap-4 px-4 text-xs font-bold",
+				"text-white/60 uppercase tracking-wider mb-2"
+			].join(' ')}>
+				<span>
+					opponent
+				</span>
+				<span className="text-center">
+					date
+				</span>
+				<span className="text-center">
+					score
+				</span>
+				<span className="text-right">
+					outcome
+				</span>
+			</div>
+		</div>
+	);
+}
+
+export default HistoryHeader;
